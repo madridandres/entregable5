3528825f797a9b7bbc27f7344f8e5d6d411460d2
madridandres <madridandres.107.am@outlook.com>
2022-11-22 00:52:15 -0500

    almost all features completed

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,18 +1,37 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import PokemonCard from './PokemonCard';
 
 const Pokedex = () => {
 
     const userName = useSelector(state => state.name);
     const [characters, setCharacters] = useState([]);
+    const [pokemonName, setPokemonName] = useState("");
+    const [types, setTypes] = useState([]);
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios.get("https://pokeapi.co/api/v2/pokemon") //Para quitar el limite
                                                         ///?offset=0&limit=1154
             .then(res => setCharacters(res.data.results));
+
+        axios.get("https://pokeapi.co/api/v2/type")
+            .then(res => setTypes(res.data.results))
     }, [])
+
+
+    const searchPokemon = () => {
+        navigate(`/pokedex/${pokemonName.toLowerCase()}`)
+    }
+
+    const filterType = (e) => {
+        const url = e.target.value;
+        axios.get(url)
+            .then(res => setCharacters(res.data.pokemon))
+    }
 
     console.log(characters)
 
@@ -20,9 +39,33 @@ const Pokedex = () => {
         <div>
             <h1>Pokedex</h1>
             <p>Welcome {userName}!</p>
+            <div>
+                <input 
+                    type="text" 
+                    placeholder='Search pokemon'
+                    value={pokemonName}
+                    onChange={e => setPokemonName(e.target.value)}
+                />
+                <button onClick={searchPokemon}>
+                    Search
+                </button>
+                <select className="selected" onChange={filterType} name="" id="">
+                    {types?.map(type => (
+                        <option 
+                           value={type.url} 
+                           key={type.name}
+                        >
+                            {type.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
             <ul>
-                {characters.map(character => (
-                    <PokemonCard url={character.url}/>
+                {characters?.map(pokemon => (
+                    <PokemonCard 
+                        url={pokemon.url ? pokemon.url : pokemon.pokemon?.url} 
+                        key={pokemon.url ? pokemon.url : pokemon.pokemon?.url}
+                    />
                  ))}
             </ul>
         </div>
--- src/components/PokedexDetail.jsx
+++ src/components/PokedexDetail.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const PokedexDetail = () => {
+
+    const [pokemon, setPokemon] = useState({})
+    const { id } = useParams();
+
+    useEffect(() => {
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+            .then(res => setPokemon(res.data))
+    }, [id])
+
+    console.log(pokemon)
+
     return (
         <div>
-            <h1>Pokemon</h1>
-            Pokedex Detail
+            <h1>{pokemon.name}</h1>
+            <img src={pokemon.sprites?.other.dream_world.front_default} alt="" />
         </div>
     );
 };
